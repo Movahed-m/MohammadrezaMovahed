aabcda5a7f7c9cce714fd3ed0e521a07705b6ee8
Mohammad Movahed <mohammad03@yahoo.com>
2024-09-15 10:32:39 +0330

    adding styles of Layout and index styles

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,26 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 
+import styles from "../styles/Layout.module.css";
+
 const Layout = ({ children }) => {
   return (
-    <>
-      <header>
+    <div className={styles.container}>
+      <header className={styles.header}>
         <h1>
           <a href="../../index.html">Cities</a>
         </h1>
       </header>
       {children}
-      <footer>
+      <footer className={styles.footer}>
         <p>
-          Developed by <span>Mohammad Movahed</span> in botostart bootcamp
+          Developed by <span className={styles.name}>Mohammad Movahed</span> in
+          botostart bootcamp
         </p>
-        <p>
-          more in{" "}
+        <p className={styles.githubContainer}>
+          more in
           <a href="https://github.com/Movahed-m">
             <FaGithub />
           </a>
         </p>
       </footer>
-    </>
+    </div>
   );
 };
 
